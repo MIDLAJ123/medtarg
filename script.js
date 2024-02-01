7c46add78be69c38a8987fbb983df58a900615aa
MUHAMMED MIDLAJ <midumidlaj9061@gmail.com>
2024-02-01 11:28:20 +0530

    up

--- script.js
+++ script.js
@@ -28,3 +28,21 @@ function changeSwitchText(el){
   switchText.children[1].innerText = showText[el].byline;
   switchButton.innerText = showText[el].buttonText;
 }
+
+$(function() {
+  var list = $('.js-dropdown-list');
+  var link = $('.js-link');
+  link.click(function(e) {
+    e.preventDefault();
+    list.slideToggle(200);
+  });
+  list.find('li').click(function() {
+    var text = $(this).html();
+    var icon = '<i class="fa fa-chevron-down"></i>';
+    link.html(text+icon);
+    list.slideToggle(200);
+    if (text === '* Reset') {
+      link.html('Select one option'+icon);
+    }
+  });
+});
